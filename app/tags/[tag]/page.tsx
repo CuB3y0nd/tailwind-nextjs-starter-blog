4f7fe2141132f0191f05ce705f6080fa495d3dfd
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2024-01-27 13:46:40 +0800

    Fix title

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -40,14 +40,7 @@ export default function TagPage({ params }: { params: { tag: string } }) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700 mt-20">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {title}
-          </h1>
-        </div>
-        <div className="flex">
-          <ListLayout posts={filteredPosts} title={title} />
-        </div>
+        <ListLayout posts={filteredPosts} title={title} />
       </div>
     </>
   )
