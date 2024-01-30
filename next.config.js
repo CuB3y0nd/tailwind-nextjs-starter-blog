db7afa432e45ea891680b940575b4d0cc0687feb
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2024-01-30 15:27:49 +0800

    Fix CORS header 'Access-Control-Allow-Origin' missing

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.cubeyond.net;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
-  connect-src *;
+  connect-src * analytics.cubeyond.net;
   font-src 'self';
   frame-src giscus.app
 `
@@ -51,6 +51,14 @@ const securityHeaders = [
   {
     key: 'Permissions-Policy',
     value: 'camera=(), microphone=(), geolocation=()',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
   },
 ]
 
