1dc2e960505245b2e353dc15f5005fff124becce
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2024-06-08 12:34:19 +0800

    feat: github alert

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
+import 'remark-github-blockquote-alert/alert.css'
 
 import '@fontsource/lxgw-wenkai'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -7,6 +7,7 @@ import { fromHtmlIsomorphic } from 'hast-util-from-html-isomorphic'
 // Remark packages
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
+import { remarkAlert } from 'remark-github-blockquote-alert'
 import {
   remarkExtractFrontmatter,
   remarkCodeTitles,
@@ -155,6 +156,7 @@ export default makeSource({
       remarkCodeTitles,
       remarkMath,
       remarkImgToJsx,
+      remarkAlert,
     ],
     rehypePlugins: [
       rehypeSlug,
